d4cfe6cfba56b38a464f8479d4d2d0ccb25c27be
keerthanaanil510 <keerthanaanil510@gmail.com>
2026-05-22 14:11:20 +0530

    added user registration feature

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./db");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Backend Server Running");
 });
+
 app.get("/users", (req, res) => {
 
     const sql = "SELECT * FROM users";
@@ -21,6 +27,35 @@ app.get("/users", (req, res) => {
     });
 
 });
+
+app.post("/users", (req, res) => {
+
+    console.log(req.body);
+
+    const { full_name, email, password, role } = req.body;
+
+    const sql = `
+        INSERT INTO users(full_name, email, password, role)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    db.query(
+        sql,
+        [full_name, email, password, role],
+        (err, result) => {
+
+            if (err) {
+                console.log(err);
+                res.send("Error adding user");
+            } else {
+                res.send("User added successfully");
+            }
+
+        }
+    );
+
+});
+
 app.listen(5000, () => {
     console.log("Server running on port 5000");
 });
